887ebbddf6fa3d5bccd396a2e26160264db38f41
Muneeb Ur Rehman <rehman9753111@gmail.com>
2026-07-20 20:49:05 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/muneeb/parkinglot/ParkingLotManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.muneeb.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingLotManagementSystemApplication.class, args);
+    }
+
+}
